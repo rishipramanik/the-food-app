0c0f1c305420e0739fe0d706d9ad6d77713363dd
rishipramanik <rishi.holmes@gmail.com>
2023-12-23 22:29:31 +0530

    connected db, added authentication using auth.js

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import Image from "../../node_modules/next/image";
 import Link from "../../node_modules/next/link";
 import CartIcon from "./CartIcon";
 import Menu from "./Menu";
+import UserLinks from "./UserLinks";
 
 function Navbar() {
   const user = false;
@@ -28,11 +29,7 @@ function Navbar() {
           <Image src="/phone.png" alt="" width={20} height={20} />
           <span>123 456 789</span>
         </div>
-        {!user ? (
-          <Link href="/login">Login</Link>
-        ) : (
-          <Link href="/orders">Orders</Link>
-        )}
+       <UserLinks />
         <CartIcon />
       </div>
     </div>
