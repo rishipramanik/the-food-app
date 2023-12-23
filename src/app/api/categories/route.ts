7cf5362219d673dfe5c7fd50b9befa93d90bc4e1
rishipramanik <rishi.holmes@gmail.com>
2023-12-23 22:51:51 +0530

    Revert "Revert "connected db, added authentication using auth.js""
    This reverts commit a0df5182cf4f9eec36efb2fdc4626cd0ef307b83.
    
    "fix revert files"

---
+++ src/app/api/categories/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "../../../../node_modules/next/server";
+import { prisma } from "@/utils/connect";
+
+//FETCH ALL CATEGORIES
+export const GET = async () => {
+  try {
+    const categories = await prisma.category.findMany();
+    return new NextResponse(JSON.stringify(categories), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }),
+      { status: 500 }
+    );
+  }
+};
--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,8 +1,22 @@
-import { menu } from "@/data";
+import { MenuType } from "@/types/types";
 import React from "react";
 import Link from "../../../node_modules/next/link";
 
-function MenuPage() {
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/categories", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed!");
+  }
+
+  return res.json();
+};
+
+const MenuPage = async () => {
+  const menu: MenuType = await getData();
+
   return (
     <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md-h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
       {menu.map((category) => (
@@ -27,6 +41,6 @@ function MenuPage() {
       ))}
     </div>
   );
-}
+};
 
 export default MenuPage;
